9a75bf5902e3685f99a53d64148de803554bdfcd
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-06-06 15:20:32 +0530

    deployment in progress

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import gameRoutes from './routes/gameRoutes.js';
+import path from "path";
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/game', gameRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
 
 app.get('/', (req, res) => {
     res.send('Dice Game API');
